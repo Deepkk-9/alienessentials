7b55480eab3ddf65101b8720d57d439a3562443a
Deep <kawaledeep9@gmail.com>
2024-01-29 22:17:46 +0530

    Frontend - Added T-shirts page with dummmy data and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,24 +5,24 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
     return (
-        <div className='flex justify-center flex-col items-center md:flex-row md:justify-start'>
+        <div className='flex justify-center flex-col items-center md:flex-row md:justify-start shadow-md mb-2'>
             <div className="logo mx-5">
-                <Image src={"/logo.jpg"} width={70} height={70} />
+                <Link href={"/"}> <Image src={"/logo.jpg"} width={70} height={70} /></Link>
             </div>
 
             <div className="nav">
-                <ul className='flex items-center space-x-2 font-bold md:text-xl'>
-                    <Link href={"/"}><li>T-shirts</li></Link>
-                    <Link href={"/"}><li>Hoodies</li></Link>
-                    <Link href={"/"}><li>Stickers</li></Link>
-                    <Link href={"/"}><li>Mugs</li></Link>
+                <ul className='flex items-center space-x-6 font-bold md:text-md'>
+                    <Link href={"/tshirts"}><li>T-shirts</li></Link>
+                    <Link href={"/hoodies"}><li>Hoodies</li></Link>
+                    <Link href={"/stickers"}><li>Stickers</li></Link>
+                    <Link href={"/mugs"}><li>Mugs</li></Link>
                 </ul>
             </div>
 
             <div className="cart absolute right-0 mx-5">
-                <AiOutlineShoppingCart className='text-2xl md:text-4xl' />
+                <AiOutlineShoppingCart className='text-xl md:text-2xl' />
             </div>
-        </div>
+        </div >
     )
 }
 
