4716ae78654fcf223b697bee22d9e0fad8304e29
Deep <kawaledeep9@gmail.com>
2024-01-30 23:52:17 +0530

    Frontend - Minor bugs solved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,22 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import React, { useRef } from 'react'
+import { AiFillCloseCircle, AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
+
+
+    const ref = useRef()
+
+    const toggleCart = () => {
+
+    }
+
+
     return (
         <div className='flex justify-center flex-col items-center md:flex-row md:justify-start shadow-md mb-2'>
             <div className="logo mx-5">
-                <Link href={"/"}> <Image src={"/logo.jpg"} width={70} height={70} /></Link>
+                <Link href={"/"}> <Image src={"/logo.jpg"} alt='logo' width={70} height={70} /></Link>
             </div>
 
             <div className="nav">
@@ -21,6 +30,19 @@ const Navbar = () => {
 
             <div className="cart absolute right-0 mx-5">
                 <AiOutlineShoppingCart className='text-xl md:text-2xl' />
+            </div>
+
+
+            <div ref={ref} className="sideCart absolute top-0 right-0 bg-green-100 p-10 transform transition-transform -translate-x-full ">
+                <h2 className='font-bold text-xl'>Shopping Cart</h2>
+                <span className="absolute top-4 right-4 cursor-pointer text-2xl text-green-500" onClick={toggleCart}>
+                    <AiFillCloseCircle />
+                </span>
+                <ol>
+                    <li>
+                        T-Shirt Wear the code
+                    </li>
+                </ol>
             </div>
         </div >
     )
